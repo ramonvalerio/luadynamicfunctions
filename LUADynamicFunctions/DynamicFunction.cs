54422e90f0c132ee0db38a2f7110d7a67bd72c98
Ramon Valerio <ramonvalerios@gmail.com>
2018-03-14 03:23:26 -0300

    first commit

---
+++ LUADynamicFunctions/DynamicFunction.cs
@@ -0,0 +1,54 @@
+﻿using NLua;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LUADynamicFunctions
+{
+    public class DynamicFunction
+    {
+        private Dictionary<string, string> _functions;
+        public TimeSpan TimeResult { get; set; }
+
+        public DynamicFunction()
+        {
+            _functions = new Dictionary<string, string>();
+        }
+
+        public void AddFunction(string functionName, string expression)
+        {
+            var result = $@"function {functionName}(x)
+                            {expression}
+                        end";
+
+            _functions.Add(functionName, result);
+        }
+
+        public IList<decimal> Execute(IEnumerable<decimal> collection)
+        {
+            var watch = Stopwatch.StartNew();
+
+            var result = new List<decimal>();
+
+            using (var state = new Lua())
+            {
+                foreach (var functionName in _functions.Keys)
+                {
+                    state.DoString(_functions[functionName]);
+                    var function = state[functionName] as LuaFunction;
+
+                    foreach (var item in collection)
+                    {
+                        result.Add(Convert.ToDecimal(function.Call(item).First()));
+                    }
+                }
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.Elapsed;
+
+            return result;
+        }
+    }
+}
---
+++ LUADynamicFunctions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LUADynamicFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var functionA = "FuncaoA";
+            var expressionA = @"if x mod 2 then
+                                    return x * 2
+                                else
+                                    return x * 100
+                                end";
+
+            var data = new List<decimal> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var dynamicFunction = new DynamicFunction();
+            dynamicFunction.AddFunction(functionA, expressionA);
+            var result = dynamicFunction.Execute(data);
+
+            for (int i = 0; i < result.Count; i++)
+                Console.WriteLine($"Linha {i}: {result[i]}");
+
+            Console.WriteLine($"Time Result: {dynamicFunction.TimeResult.TotalSeconds}");
+            Console.ReadKey();
+        }
+    }
+}
